5d01ccad1a35dcd8c1842b7e1c8a8e250b9d488e
bacusy <191461931+BCAUSY@users.noreply.github.com>
2025-02-12 22:33:46 +0100

    second result needs rework

--- script.js
+++ script.js
@@ -1,36 +1,21 @@
-function add(num1,num2){
-    return roundToTwo(num1 + num2);
+function add(num1, num2) {
+  return roundToTwo(num1 + num2);
 }
 
-function subtract(num1,num2){
-    return roundToTwo(num1 - num2);
+function subtract(num1, num2) {
+  return roundToTwo(num1 - num2);
 }
 
-function multiply(num1,num2){
-    return roundToTwo(num1 * num2);
+function multiply(num1, num2) {
+  return roundToTwo(num1 * num2);
 }
 
-function divide(num1, num2){
-    return roundToTwo(num1 / num2)
+function divide(num1, num2) {
+  return roundToTwo(num1 / num2);
 }
 
 function roundToTwo(num) {
   return +(Math.round(num + "e+2") + "e-2");
-}
-
-function operate(num1,operator,num2){
-    if (operator === `+`){
-        return add(num1, num2);
-    } 
-    else if (operator === "-") {
-        return subtract(num1, num2);
-    }
-    else if (operator === "/"){
-        return divide(num1, num2);
-    }
-    else if (operator === "*"){
-        return multiply(num1, num2);
-    }
 }
 
 const digitOne = document.getElementById("num1");
@@ -40,41 +25,124 @@ const digitSum = document.getElementById("result");
 
 const keyPad = document.querySelectorAll(".pad");
 const digClass = document.querySelectorAll(".digit");
-const deleteButton = document.getElementById("delete")
+const deleteButton = document.getElementById("delete");
 const clearButton = document.getElementById("clearButton");
 const operator = document.querySelectorAll(".op");
+const sum = document.getElementById("equalsButton");
+let result = [];
 
-clearButton.addEventListener("click",clear);
+clearButton.addEventListener("click", clear);
 let clearTrigger = false;
-keyPad.forEach (button => {
-    button.addEventListener("click", function(){
-        if (!clearTrigger) {
-            clear();
-            clearTrigger = true;
-        }
-       const buttonValue = button.value;
-       digitOne.innerText += buttonValue;
-        console.log(buttonValue);
-    });
-})
 
-deleteButton.addEventListener("click", function(){
-        digitOne.innerText = digitOne.innerText.slice(0, (digitOne.innerText.length - 1))
-    })
+let numOneTrigger = false;
+let numTwoTrigger = false;
 
+let finNumOne = 0;
+let finNumTwo = 0;
+let finOperator = 0;
+let finSum = 0;
+function backspace() {
+  if (numTwoTrigger) numOneTrigger = false;
+  if (numOneTrigger) {
+    digitOne.innerText = digitOne.innerText.slice(
+      0,
+      digitOne.innerText.length - 1
+    );
+    finNumOne = Number(digitOne.innerText);
+  }
+  if (numTwoTrigger) {
+    digitTwo.innerText = digitTwo.innerText.slice(
+      0,
+      digitTwo.innerText.length - 1
+    );
+    finNumTwo = Number(digitTwo.innerText);
+  }
+}
+console.log(numOneTrigger);
 
-function clear(){
-    digitOne.innerText = "";
-    digitOperator.innerText = "";
-    digitTwo.innerText = "";
-    digitSum.innerText = "";
-        
+function clear() {
+  finNumOne = 0;
+  finNumTwo = 0;
+  finOperator = 0;
+  finSum = 0;
+  numOneTrigger = false;
+  numTwoTrigger = false;
+
+  keyPad.forEach((button) => {
+    button.removeEventListener("click", numTwoIn);
+  });
+  digitOne.innerText = "";
+  digitOperator.innerText = "";
+  digitTwo.innerText = "";
+  digitSum.innerText = "";
+  keyPad.forEach((button) => {
+    button.addEventListener("click", numIn);
+  });
 }
 
-operator.forEach(button => {
-    button.addEventListener("click", function(){
-        
-        const operatorValue = button.value;
-        digitOperator.innerText = operatorValue
-    })
-})
+keyPad.forEach((button) => {
+  button.addEventListener("click", numIn);
+});
+
+function numIn() {
+  deleteButton.addEventListener("click", backspace);
+  if (!clearTrigger) {
+    clear();
+    clearTrigger = true;
+  }
+  const buttonValue = this.value;
+  digitOne.innerText += buttonValue;
+  numOneTrigger = true;
+  finNumOne = Number(digitOne.innerText);
+}
+
+function numTwoIn() {
+  numTwoTrigger = true;
+  const buttonValue = this.value;
+  digitTwo.innerText += buttonValue;
+  finNumTwo = Number(digitTwo.innerText);
+}
+
+function operIn() {
+  digitOperator.innerText = this.value;
+  finOperator = digitOperator.innerText = this.value;
+  if (numOneTrigger) {
+    keyPad.forEach((button) => {
+      button.removeEventListener("click", numIn);
+    });
+    keyPad.forEach((button) => {
+      button.addEventListener("click", numTwoIn);
+    });
+  }
+}
+function operateMore(num1, operator, num2) {
+    
+
+}
+
+operator.forEach((button) => {
+  button.addEventListener("click", operIn);
+});
+
+function operate(num1, operator, num2) {
+  if (operator === `+`) {
+    finSum = add(num1, num2);
+  } else if (operator === "-") {
+    finSum = subtract(num1, num2);
+  } else if (operator === "÷") {
+    finSum = divide(num1, num2);
+  } else if (digitOperator.innerText === "×") {
+    finSum = multiply(num1, num2);
+  } 
+}
+
+let history = [];
+
+function getFinSum() {
+  deleteButton.removeEventListener("click", backspace);
+  operate(finNumOne, finOperator, finNumTwo);
+  digitSum.innerText = finSum;
+  result = [finNumOne + finOperator + finNumTwo + "=" + finSum];
+  history.push(result);
+}
+sum.addEventListener("click", getFinSum);
